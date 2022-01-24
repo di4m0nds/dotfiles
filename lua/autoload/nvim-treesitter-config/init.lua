77e40c8697bddc0c70456dec38d7465e6c89c25d
Javi-bit <ws.2000.sja@gmail.com>
2022-01-24 01:59:15 -0300

    Treesitter Plugin

---
+++ lua/autoload/nvim-treesitter-config/init.lua
@@ -0,0 +1,10 @@
+-- Plugin <Nvim Treesitter> 
+-- GitHub Repository https://github.com/nvim-treesitter/nvim-treesitter
+-- 
+-- << Nvim Treesitter Configuration >>
+
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true,
+  },
+}
