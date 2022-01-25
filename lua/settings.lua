15c588c8b4ba82c6f0f60ef8ef695df79ef378cb
Javi-bit <ws.2000.sja@gmail.com>
2022-01-25 14:59:03 -0300

    Updated Settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,8 @@
 --	Neovim Settings
 
-local cmd = vim.cmd	-- Vim commands
-local g = vim.g		-- Vim Global Variables
-local opt = vim.opt	-- global/buffer/windows-scoped options
+local cmd = vim.cmd -- Vim commands
+local g = vim.g     -- Vim Global Variables
+local opt = vim.opt -- global/buffer/windows-scoped options
 
 opt.mouse = 'a'               -- enable mouse support
 opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
@@ -12,7 +12,7 @@ opt.number = true             -- show line number
 cmd('set relativenumber')     -- relative number
 opt.showmatch = true          -- highlight matching parenthesis
 opt.splitright = true         -- vertical split to the right
-opt.splitbelow = true         -- orizontal split to the bottom
+opt.splitbelow = true         -- horizontal split to the bottom
 opt.ignorecase = true         -- ignore case letters when search
 opt.smartcase = true          -- ignore lowercase for the whole pattern
 opt.linebreak = true          -- wrap on word boundary
@@ -25,6 +25,6 @@ opt.synmaxcol = 240       -- max column for syntax highlight
 opt.termguicolors = true      -- enable 24-bit RGB colors
 
 opt.expandtab = true      -- use spaces instead of tabs
-opt.shiftwidth = 2        -- shift 4 spaces when tab
-opt.tabstop = 2           -- 1 tab == 4 spaces
+opt.shiftwidth = 2
+opt.tabstop = 2
 opt.smartindent = true    -- autoindent new lines
