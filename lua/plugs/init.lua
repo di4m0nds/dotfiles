fa12919c0dc6ef61ec4740cf77f246a7e5f3b885
di4m0nds <ws.2000.sja@gmail.com>
2022-01-28 14:09:23 -0300

    ⏸️ Added Package << Markdown Preview Nvim >>

--- lua/plugs/init.lua
+++ lua/plugs/init.lua
@@ -70,6 +70,14 @@ function init()
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  -- Markdown Preview
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" }
+  })
+
   -- Dashboard
   use 'goolord/alpha-nvim'
 
