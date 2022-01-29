b594558e61ecef00f102243121a19382dcfc4484
di4m0nds <ws.2000.sja@gmail.com>
2022-01-29 03:20:04 -0300

    👉 Added a little config for gruvbox

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,7 +1,18 @@
 -- Colorschemes Configuration
 
--- Set theme
-vim.cmd('colorscheme gruvbox')
+local g = vim.g
+local cmd = vim.cmd
+
+cmd[[set background=dark]]
+
+-- << Configuration for Themes >>
+-- Theme: Gruvbox
+g.gruvbox_bold = 1
+g.gruvbox_italic = 1
+
+-- << Set theme >>
+-- Themes open: rose-pine, gruvbox and material
+cmd[[colorscheme gruvbox]]
 
 -- Transparent Background (Theme)
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+cmd[[hi Normal guibg=NONE ctermbg=NONE]]
