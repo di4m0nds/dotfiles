b32e0f0998b62f16d5203e7169bab490755078af
di4m0nds <ws.2000.sja@gmail.com>
2022-02-03 10:26:48 -0300

    Modified Dashboard for Neovim
    I created new dashboard with alpha config

--- lua/autoload/nvim-alpha-config/init.lua
+++ lua/autoload/nvim-alpha-config/init.lua
@@ -3,6 +3,56 @@
 -- 
 -- << Dashboard Configuration >>
 
-local alpha = require 'alpha'
+--local alpha = require 'alpha'
+--
+--alpha.setup(require'alpha.themes.dashboard'.config)
 
-alpha.setup(require'alpha.themes.dashboard'.config)
+local dashboard = require 'alpha.themes.dashboard'
+
+-- Header Section
+local ne0vim = {
+  [[                           ___---___                                ]],
+  [[                        .--         --.                             ]],
+  [[                      ./   ()      .-. \.                           ]],
+  [[                     /   o    .   (   )  \                          ]],
+  [[                    / .            '-'    \                         ]],
+  [[███╗   ██╗███████╗ | ()    .  O         .  | ██╗   ██╗██╗███╗   ███╗]],
+  [[████╗  ██║██╔════╝|                         |██║   ██║██║████╗ ████║]],
+  [[██╔██╗ ██║█████╗  |    o           ()       |██║   ██║██║██╔████╔██║]],
+  [[██║╚██╗██║██╔══╝  |       .--.          O   |╚██╗ ██╔╝██║██║╚██╔╝██║]],
+  [[██║ ╚████║███████╗ | .   |    |            |  ╚████╔╝ ██║██║ ╚═╝ ██║]],
+  [[╚═╝  ╚═══╝╚══════╝  \    `.__.'    o   .  /    ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+  [[                     \                   /                          ]],
+  [[                      `\  o    ()      /'                           ]],
+  [[                        `--___   ___--'                             ]],
+  [[                              ---                                   ]],
+}
+dashboard.section.header.val = ne0vim
+
+-- Menu Section
+dashboard.section.buttons.val = {
+  dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+  dashboard.button('f', '  Search file', ':Telescope find_files<CR>'),
+  dashboard.button('--', '- - - - - - - - - - - - - - - - - - - - - - -', ':<CR>'),
+  dashboard.button('s', '  Go Init Lua', ':e $MYVIMRC<CR>'),
+  dashboard.button('u', '  Update Packer Packages', ':PackerUpdate<CR>'),
+  dashboard.button('q', '  Exit', ':qa<CR>'),
+}
+
+-- Footer Section
+local footer = {
+  [[]],
+  os.date('%Y/%m/%d - %H:%M:%S'),
+  [[███████████████████████████████████████████████████████████████]],
+  [[█▄─▄─▀█▄─█─▄███▄─▄▄▀█▄─▄█░█░██▄─▀█▀─▄█─▄▄─█▄─▀█▄─▄█▄─▄▄▀█─▄▄▄▄█]],
+  [[██─▄─▀██▄─▄█████─██─██─██▄▄░███─█▄█─██─██─██─█▄▀─███─██─█▄▄▄▄─█]],
+  [[▀▄▄▄▄▀▀▀▄▄▄▀▀▀▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀]],
+}
+dashboard.section.footer.val = footer
+
+
+
+
+
+
+require'alpha'.setup(dashboard.config)
